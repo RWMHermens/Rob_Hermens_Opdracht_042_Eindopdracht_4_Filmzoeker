bfdbc250fe3d95eb3247adfb42d27ef5f4ceb0ab
Rob Hermens <robhermens@gmail.com>
2021-07-06 16:07:59 +0200

    Single function for various superheroes

--- scripts/script.js
+++ scripts/script.js
@@ -28,43 +28,51 @@ const removeMovies = () => {
   }
 };
 
-const filterAvengersMovies = () => {
+const filterMovies = (filter) => {
   removeMovies();
-  const filteredAvengers = movies.filter((movie) =>
-    movie.Title.includes("Avengers")
-  );
-  filteredAvengers.forEach((movie) => {
-    addMoviesToDom(movie);
+  const filteredMovies = movies.filter((m) => m.Title.includes(filter));
+  filteredMovies.forEach((m) => {
+    addMoviesToDom(m);
   });
 };
 
-const filterXmenMovies = () => {
-  removeMovies();
-  const filteredXmen = movies.filter((movie) => movie.Title.includes("X-Men"));
-  filteredXmen.forEach((movie) => {
-    addMoviesToDom(movie);
-  });
-};
+// const filterAvengersMovies = () => {
+//   removeMovies();
+//   const filteredAvengers = movies.filter((movie) =>
+//     movie.Title.includes("Avengers")
+//   );
+//   filteredAvengers.forEach((movie) => {
+//     addMoviesToDom(movie);
+//   });
+// };
 
-const filterPrincessMovies = () => {
-  removeMovies();
-  const filteredPrincess = movies.filter((movie) =>
-    movie.Title.includes("Princess")
-  );
-  filteredPrincess.forEach((movie) => {
-    addMoviesToDom(movie);
-  });
-};
+// const filterXmenMovies = () => {
+//   removeMovies();
+//   const filteredXmen = movies.filter((movie) => movie.Title.includes("X-Men"));
+//   filteredXmen.forEach((movie) => {
+//     addMoviesToDom(movie);
+//   });
+// };
 
-const filterBatmanMovies = () => {
-  removeMovies();
-  const filteredBatman = movies.filter((movie) =>
-    movie.Title.includes("Batman")
-  );
-  filteredBatman.forEach((movie) => {
-    addMoviesToDom(movie);
-  });
-};
+// const filterPrincessMovies = () => {
+//   removeMovies();
+//   const filteredPrincess = movies.filter((movie) =>
+//     movie.Title.includes("Princess")
+//   );
+//   filteredPrincess.forEach((movie) => {
+//     addMoviesToDom(movie);
+//   });
+// };
+
+// const filterBatmanMovies = () => {
+//   removeMovies();
+//   const filteredBatman = movies.filter((movie) =>
+//     movie.Title.includes("Batman")
+//   );
+//   filteredBatman.forEach((movie) => {
+//     addMoviesToDom(movie);
+//   });
+// };
 
 const filterLatestMovies = () => {
   removeMovies();
@@ -84,16 +92,16 @@ radioButtons.forEach((button) =>
         filterLatestMovies();
         break;
       case "avengers":
-        filterAvengersMovies();
+        filterMovies("Avengers");
         break;
       case "xmen":
-        filterXmenMovies();
+        filterMovies("X-Men");
         break;
       case "princess":
-        filterPrincessMovies();
+        filterMovies("Princess");
         break;
       case "batman":
-        filterBatmanMovies();
+        filterMovies("Batman");
         break;
     }
   })
